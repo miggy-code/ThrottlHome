7c832d559bb9510288cf90860c245da66c7db57b
Miguel <meeg@Miguels-MacBook-Pro.local>
2026-04-02 14:10:33 -0500

    refactor(home): remove manifesto section and reorder flow (Problem -> Services -> Philosophy -> Process)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import dynamic from "next/dynamic";
 import { HeroSection } from "@/components/home/HeroSection";
 
 const ProblemSection = dynamic(() => import("@/components/home/ProblemSection").then((m) => m.ProblemSection));
-const PhilosophySection = dynamic(() => import("@/components/home/PhilosophySection").then((m) => m.PhilosophySection));
-const ManifestoSection = dynamic(() => import("@/components/home/ManifestoSection").then((m) => m.ManifestoSection));
 const ServicesSection = dynamic(() => import("@/components/home/ServicesSection").then((m) => m.ServicesSection));
+const PhilosophySection = dynamic(() => import("@/components/home/PhilosophySection").then((m) => m.PhilosophySection));
 const ProcessSection = dynamic(() => import("@/components/home/ProcessSection").then((m) => m.ProcessSection));
 const ReadinessAssessment = dynamic(() => import("@/components/home/ReadinessAssessment").then((m) => m.ReadinessAssessment));
 const ProofSection = dynamic(() => import("@/components/home/ProofSection").then((m) => m.ProofSection));
@@ -15,9 +14,8 @@ export default function Home() {
     <>
       <HeroSection />
       <ProblemSection />
-      <PhilosophySection />
-      <ManifestoSection />
       <ServicesSection />
+      <PhilosophySection />
       <ProcessSection />
       <ReadinessAssessment />
       <ProofSection />
